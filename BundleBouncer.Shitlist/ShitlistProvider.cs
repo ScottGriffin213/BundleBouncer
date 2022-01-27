d6bd2d5932f167356909dbacdb2e4bf4058644fc
Scottinator <scgriffin213@gmail.com>
2022-01-26 17:04:02 -0800

    v1.2.2

--- BundleBouncer.Shitlist/ShitlistProvider.cs
+++ BundleBouncer.Shitlist/ShitlistProvider.cs
@@ -7,7 +7,7 @@ namespace BundleBouncer.Shitlist
     [System.CodeDom.Compiler.GeneratedCode("devtools/build.py", "0.0.0")]
     public class ShitlistProvider : IShitListProvider
     {
-        public ShitlistProvider() => Logging.Info("BundleBouncer definitions generated @ 2022-01-25T23:09:43.966146");
+        public ShitlistProvider() => Logging.Info("BundleBouncer definitions generated @ 2022-01-27T01:03:38.590767");
 
         // The following is a bunch of generated if-trees created by putting
         // a bunch of avID SHA256s into a trie (https://en.wikipedia.org/wiki/Trie) and optimizing it.
