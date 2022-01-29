f67981fd9f490b2fcd9bd29504a83be3903376ca
Scottinator <scgriffin213@gmail.com>
2022-01-28 19:05:54 -0800

    Specify deps.

--- BundleBouncer/AssemblyInfo.cs
+++ BundleBouncer/AssemblyInfo.cs
@@ -11,13 +11,15 @@ using System.Runtime.InteropServices;
 // Setting ComVisible to false makes the types in this assembly not visible to COM
 // components.  If you need to access a type in this assembly from COM, set the ComVisible
 // attribute to true on that type.
-
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM.
-
 [assembly: Guid("E22130A0-1166-448B-B4D9-B4E3AC7E305E")]
+
 [assembly: MelonInfo(typeof(BundleBouncer.BundleBouncer), "BundleBouncer", "1.2.3", "Scott Griffin")]
 [assembly: MelonGame("VRChat", "VRChat")]
 [assembly: MelonColor(ConsoleColor.Red)]
+[assembly: MelonAdditionalDependencies("VRChatUtilityKit", "UIExpansionKit")]
+[assembly: MelonPlatform(MelonPlatformAttribute.CompatiblePlatforms.WINDOWS_X64)]
+[assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
 // Broken in 0.5.3.0 [assembly: VerifyLoaderVersion(0, 5, 3, true)] // Bug in ML 0.5.2 results in false obfuscation detection.
