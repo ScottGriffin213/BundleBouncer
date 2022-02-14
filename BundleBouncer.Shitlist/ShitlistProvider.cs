0f70ae7569ddf1e565028c33e07e3db03cc71f5e
Scottinator <scgriffin213@gmail.com>
2022-02-13 19:45:11 -0800

    More YARA rules.

--- BundleBouncer.Shitlist/ShitlistProvider.cs
+++ BundleBouncer.Shitlist/ShitlistProvider.cs
@@ -43,7 +43,7 @@ namespace BundleBouncer.Shitlist
             Logging.Info($"  Whitelisted AssetBundle Hashes.: {LEN_WHITELISTED_HASHES}");
         }
 
-        public const string GENERATED_AT = "2022-02-14T03:41:36.357897";
+        public const string GENERATED_AT = "2022-02-14T03:45:06.475568";
 
         public const int LEN_BLACKLISTED_AVIDS = 39;
         public const int LEN_WHITELISTED_AVIDS = 1;
