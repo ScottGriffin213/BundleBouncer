ff0486a9a06b58a26ad25386014ab7b6900e412c
Scottinator <scgriffin213@gmail.com>
2022-02-15 13:23:57 -0800

    Check lengths before proceeding into if-tree.  Prevents avatars that use a whitelisted prefix.

--- BundleBouncer.Shitlist/ShitlistProvider.cs
+++ BundleBouncer.Shitlist/ShitlistProvider.cs
@@ -43,7 +43,7 @@ namespace BundleBouncer.Shitlist
             Logging.Info($"  Whitelisted AssetBundle Hashes.: {LEN_WHITELISTED_HASHES}");
         }
 
-        public const string GENERATED_AT = "2022-02-15T21:20:52.367801";
+        public const string GENERATED_AT = "2022-02-15T21:23:50.351417";
 
         public const int LEN_BLACKLISTED_AVIDS = 40;
         public const int LEN_WHITELISTED_AVIDS = 1;
